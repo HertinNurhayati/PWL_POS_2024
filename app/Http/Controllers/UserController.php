e347a3a041e48d7346125f1308bd1e0b73b16f94
hertin nurhayati <hertinnurhayati4@gmail.com>
2024-09-20 09:28:26 +0700

    Praktikum 2.2 (bab 4)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,10 +22,14 @@ class UserController extends Controller
 
         // Ambil user pertama dengan level_id = 1
         /*$user = UserModel::firstWhere('level_id', 1);*/
-        
-            $user = UserModel::findOr (20, ['username', 'nama'], function (){
+
+        /*    $user = UserModel::findOr (20, ['username', 'nama'], function (){
             abort (404);
-        });
+        });*/
+
+        /*$user = UserModel::findOrFail(1);*/
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
