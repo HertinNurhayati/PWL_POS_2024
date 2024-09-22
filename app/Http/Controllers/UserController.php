4f9ed8e582220576a8b9a7f6752ef1bacf0a3706
hertin nurhayati <hertinnurhayati4@gmail.com>
2024-09-22 21:52:33 +0700

    Praktikum 2.7 (bab 4)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,85 +8,198 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        /*$data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 2',
-            'password' => Hash::make('12345')
-        ];
+    // public function index()
+    // {
+        // tambah data user dengan Eloquent Model 
         
-        UserModel::create($data);
-        */
+        /*$data = [ 
+            'username' => 'customer-1', 
+            'nama' => 'Pelanggan', 
+            'password' => Hash::make('12345'), 
+            'level_id' => 4 
+        ]; 
+        UserModel::insert($data); // tambahkan data ke tabel m_user 
 
-        // Ambil user pertama dengan level_id = 1
-        /*$user = UserModel::firstWhere('level_id', 1);*/
+        $data = [
+            'nama'=> 'Pelanggan Pertama',
+        ];
+        userModel::where('username', 'customer-1')->update($data);*/ //update data user
+        
+        //JS4 Praktikum 1 
+	    //Langkah 2
+        /*$data = [ 
+            'level_id' => 2, 
+            'username' => 'manager_dua', 
+            'nama' => 'Manager 2', 
+            'password' => Hash::make('12345') 
+        ]; 	
+	     UserModel::create($data);*/
 
-        /*    $user = UserModel::findOr (20, ['username', 'nama'], function (){
-            abort (404);
-        });*/
+        //Langkah 5
+        /*$data = [ 
+            'level_id' => 2, 
+            'username' => 'manager_tiga', 
+            'nama' => 'Manager 3', 
+            'password' => Hash::make('12345') 
+        ]; 
+            UserModel::create($data) ;*/
+        
+        // coba akses model UserModel 
+        /*$user = UserModel::all(); // ambil semua data dari tabel m_user 
+        return view('user', ['data' => $user]); */
 
-        /*$user = UserModel::findOrFail(1);*/
-
-        /*$user = UserModel::where('username', 'manager9')->firstOrFail();*/
-
-         // Hitung jumlah pengguna dengan level_id = 2
-        /*$userCount = UserModel::where('level_id', 2)->count();
-        // Kirim jumlah pengguna ke view
-        return view('user', ['userCount' => $userCount]);*/
-
-        /*$user = UserModel::firstOrCreate(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+        //JS4 Praktikum 2.1
+        //Langkah 1
+        /*$user = UserModel::find(1);  
         return view('user', ['data' => $user]);*/
 
+        //Langkah 4
+        /*$user = UserModel::where('level_id', 1)->first();  
+        return view('user', ['data' => $user]);*/
 
+        //Langkah 6
+        /*$user = UserModel::firstwhere('level_id', 1);  
+        return view('user', ['data' => $user]);*/
 
-        /*$user = UserModel::create([
-        'username' => 'manager11',
-        'nama' => 'Manager11',
-        'password' => Hash::make('12345'),
-        'level_id' => 2,
-        ]);
+        //Langkah 8
+        /*$user = UserModel :: findOr(20, ['username', 'nama'], function () { 
+            abort(404); 
+        }); 
+        return view('user', ['data' => $user]);*/
+        
+        //JS4 Praktikum 2.2
+        //Langkah 1
+        /*$user = UserModel :: findOrFail(1); 
+        return view('user', ['data' => $user]);*/
 
-        $user->username = 'manager12';
+        //Langkah 3
+        /*$user = UserModel::where('username', 'manager9')->firstOrFail(); 
+        return view('user', ['data' => $user]);*/
 
-        /*$user->isDirty(); // true
-        $user->isDirty('username'); // true
-        $user->isDirty('nama'); // false
-        $user->isDirty (['nama', 'username']); // true
+        //JS4 Praktikum 2.3
+        //Langkah 1
+        /*$user = UserModel::where('level_id', 2)->count(); 
+        dd($user); 
+        return view('user', ['data' => $user]);*/
+        
+        //Langkah 3
+        /*$user = UserModel::where('level_id', 2)->count(); 
+        return view('user', ['data' => $user]);*/
+        
+        //JS4 Praktikum 2.4
+        //Langkah 1
+        /*$user = UserModel :: firstOrCreate( 
+            [ 
+                'username' => 'manager', 
+                'nama' => 'Manager', 
+            ], 
+        ); 
+        
+        return view('user', ['data' => $user]);*/
 
-        $user->isClean(); // false
-        $user->isClean('username'); // false
-        $user->isClean ('nama'); // true
-        $user->isClean (['nama', 'username']); // false*/
+        //Langkah 4
+        /*$user = UserModel :: firstOrCreate( 
+            [ 
+                'username' => 'manager22', 
+                'nama' => 'Manager Dua Dua', 
+                'password' => Hash::make('12345'), 
+                'level_id' => 2 
+            ], 
+        ); 
+        
+        return view('user', ['data' => $user]);*/
+        
+        //Langkah 6
+        /*$user = UserModel :: firstOrNew( 
+            [ 
+                'username' => 'manager', 
+                'nama' => 'Manager', 
+            ], 
+        ); 
+        
+        return view('user', ['data' => $user]); */
 
-        /*$user->save();
+        //Langkah 8
+        /*$user = UserModel::firstOrNew( 
+            [ 
+                'username' => 'manager33', 
+                'nama' => 'Manager Tiga Tiga', 
+                'password' => Hash::make('12345'), 
+                'level_id' => 2 
+            ], 
+        ); 
+ 
+        return view('user', ['data' => $user]); */
 
-        /*$user->isDirty(); // false
-        $user->isClean(); // true
+        //Langkah 10
+        /*$user = UserModel::firstOrNew( 
+            [ 
+                'username' => 'manager33', 
+                'nama' => 'Manager Tiga Tiga', 
+                'password' => Hash::make('12345'), 
+                'level_id' => 2 
+            ], 
+        ); 
+        $user->save(); 
+ 
+        return view('user', ['data' => $user]);*/
+
+        //JS4 Praktikum 2.5
+        //Langkah 1
+        /*$user = UserModel::create([ 
+            'username' => 'manager55', 
+            'nama' => 'Manager55', 
+            'password' => Hash::make('12345'), 
+            'level_id' => 2, 
+        ]); 
+ 
+        $user->username = 'manager56'; 
+ 
+        $user->isDirty(); // true 
+        $user->isDirty('username'); // true 
+        $user->isDirty('nama'); // false 
+        $user->isDirty(['nama', 'username']); // true 
+ 
+        $user->isClean(); // false 
+        $user->isClean('username'); // false 
+        $user->isClean('nama'); // true 
+        $user->isClean(['nama', 'username']); // false 
+ 
+        $user->save(); 
+ 
+        $user->isDirty(); // false 
+        $user->isClean(); // true 
         dd($user->isDirty());*/
 
-        /*$user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged (['username', 'level_id']); // true
-        $user->wasChanged('nama'); // false
-        dd($user->wasChanged(['nama', 'username'])); // <true></true>*/
+        //Langkah 3
+        /*$user = UserModel::create([ 
+            'username' => 'manager11', 
+            'nama' => 'Manager11', 
+            'password' => Hash::make('12345'), 
+            'level_id' => 2, 
+             
+            ]); 
+             
+            $user->username = 'manager12'; 
+             
+            $user->save() ; 
+             
+            $user->wasChanged(); // true 
+            $user->wasChanged('username'); // true 
+            $user->wasChanged(['username', 'level_id']); // true 
+            $user->wasChanged('nama'); // false 
+            dd($user->wasChanged(['nama', 'username'])); // true */
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-    }
+        //JS4 Praktikum 2.6
+    //     $user = UserModel::all();
+    //     return view('user',['data'=> $user]);
+    // }
         public function tambah()
         {
-        return view('user_tambah');
-    }
-        public function tambah_simpan(Request $request)
+            return view('user_tambah');
+        }
+    
+    public function tambah_simpan(Request $request)
     {
         UserModel::create([
             'username'=> $request->username,
@@ -104,8 +217,8 @@ class UserController extends Controller
         $user = UserModel::find($id);
         return view ('user_ubah',['data' => $user]);
     }
-
-    public function ubah_simpan(Request $request)
+    
+    public function ubah_simpan(Request $request) 
     {
         $user = UserModel::find($request->id);
 
@@ -127,6 +240,12 @@ class UserController extends Controller
 
         return redirect('/user');
     }
-    
 
+    //JS4 Praktikum 2.7
+    public function index()
+    {
+        $user = UserModel::with('level')->get();
+        //dd($user);
+        return view('user', ['data' => $user]);
+    }
 }
