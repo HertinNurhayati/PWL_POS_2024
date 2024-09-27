fb368721f1def1ef14e5fcf456f2550670c32718
hertin nurhayati <hertinnurhayati4@gmail.com>
2024-09-27 09:49:45 +0700

    Praktikum Ke-3 – Implementasi jQuery Datatable di AdminLTE (bab 5)

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +18,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::put('/user/hapus/{id}', [UserController::class, 'hapus']);
+Route::get('/level',[LevelController::class, 'index']);
+Route::get('/kategori',[KategoriController::class, 'index']);
+Route::get('/user',[UserController::class, 'index']);
+Route::get('/user/tambah',[UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan',[UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}',[UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}',[UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}',[UserController::class, 'hapus']);
+*/
+
+//Jobsheet 5
+Route::get('/', [WelcomeController::class,'index']);
+
+Route::group(['prefix' => 'user'], function(){
+    Route::get('/', [UserController::class, 'index']);          //menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);      //menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);   //Menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);         //Menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);       //menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);  //menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);     //menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);  //menghapus data user
+});
