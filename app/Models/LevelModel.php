6488f11797981f88acdcdb8f4bc4a8a8fccaff31
hertin nurhayati <hertinnurhayati4@gmail.com>
2024-09-28 19:19:34 +0700

    Tugas m_Level (bab 5)

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -2,19 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LevelModel extends Model
 {
+    protected $table = 'm_level';
+    protected $primaryKey = 'level_id';
 
-    use HasFactory;
-    protected $table = "m_level";
-    protected $primaryKey = "level_id";
+    protected $fillable = ['level_id', 'level_kode', 'level_nama'];
 
-    public function users(): HasMany
+    public function user():BelongsTo
     {
-        return $this->hasMany(User::class);
+        return $this->belongsTo(UserModel::class);
     }
 }
