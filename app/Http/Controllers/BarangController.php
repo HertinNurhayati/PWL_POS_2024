eb1c98b2bfe3368218d40001aa447392672910ca
hertin nurhayati <hertinnurhayati4@gmail.com>
2024-10-18 08:40:56 +0700

    Praktikum 3 – Implementasi Export PDF di Laravel dengan dompdf (bab 8)

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Barryvdh\DomPDF\Facade\Pdf;
 use App\Models\BarangModel;
 use App\Models\KategoriModel; // Make sure to import the KategoriModel
 use Illuminate\Http\Request;
@@ -334,6 +335,21 @@ class BarangController extends Controller
         //end function export_excel
     }
 
+    public function export_pdf()
+    {
+        $barang = BarangModel::select('kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual')
+            ->orderBy('kategori_id')
+            ->orderBy('barang_kode')
+            ->with('kategori')
+            ->get();
+
+        $pdf = Pdf::loadView('barang.export_pdf', ['barang' => $barang]);
+        $pdf->setPaper('a4', 'portrait'); //set ukuran kertas dan orientasi
+        $pdf->setOption("isRemoteEnabled", true); //set true jika ada gambar dari url
+        $pdf->render();
+
+        return $pdf->stream('Data barang ' . date('Y-m-d H:i:s') . 'pdf');
+    }
 
     public function confirm_ajax(string $id)
     {
