22a8d5ba82762e25478c14876ff09568e19cd912
hertin nurhayati <hertinnurhayati4@gmail.com>
2024-11-06 08:18:08 +0700

    Praktikum 4 – Implementasi CRUD dalam RESTful API (10)

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\LevelController;
 
-
-Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register'); 
+//Login
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
-Route::middleware('auth:api')->get('/user', function (Request $request){
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Logout
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// // });
+
+// Level
+Route:: get('levels', [LevelController::class, 'index' ]);
+Route::post('levels', [LevelController::class, 'store' ]);
+Route:: get('levels/{level}', [LevelController::class, 'show' ]);
+Route:: put('levels/{level}', [LevelController::class, 'update' ]);
+Route::delete('levels/{level}', [LevelController::class, 'destroy' ]);
